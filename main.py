e879ad701678967dbdd8b3a1762e1c750316726a
KangGeonyoung <kky6335@gmail.com>
2023-12-13 17:33:25 +0900

    Yolov5 & flask

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import subprocess
 import os
 import shutil
+import move
 from pathlib import Path
 from calculate import match_boxes
+
 
 def clear_directory(directory):
     if directory.exists() and directory.is_dir():
         shutil.rmtree(directory)
     directory.mkdir(parents=True, exist_ok=True)
 
+
 def run_yolov5_detection():
-    base_dir = Path('C:\\Users\\gram15\\smartechne\\yolov5-master\\yolov5-master')
+    base_dir = Path('C:\\Users\\User\\PycharmProjects\\yolov5-master')
     img_dir = base_dir / 'runs' / 'detect' / 'images'
     txt_dir = base_dir / 'runs' / 'detect' / 'texts'
 
@@ -34,7 +37,17 @@ def run_yolov5_detection():
         matches = match_boxes(file_path, file_seat)
         print(f"Matches for {file_path.name}: {matches}")
 
+
 run_yolov5_detection()
 
-subprocess.run(['python', 'move.py'])
+# subprocess.run(['python', 'move.py'])
 
+# API로 전달해야 하는 결과 값
+path = move.move()
+matches = match_boxes(path, 'seat.txt')
+remain_seat = 36 - len(matches)
+result = [item[1] for item in matches]
+result.sort()
+print("---------RESULT---------")
+print(result)
+print(remain_seat)
